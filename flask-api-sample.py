a349a54c139e010bb6b96027fd66c142730e31b5
Min Min <minmhan@gmail.com>
2016-07-13 22:38:53 +0800

    update rest api client

--- flask-api-sample.py
+++ flask-api-sample.py
@@ -9,22 +9,23 @@ from flask import Flask, jsonify, abort, make_response, request, url_for
 from flask.ext.httpauth import HTTPBasicAuth
 
 auth = HTTPBasicAuth()
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="")
 
 tasks = [
     {
-        'id':1,
-        'title':u'Buy groceries',
-        'description':u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': True
     },
     {
-        'id':2,
-        'title':u'Learn Python',
-        'description':u'Need to find a good python tutorial on the web',
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good python tutorial on the web',
         'done': False
     }
 ]
+
 
 @auth.get_password
 def get_password(username):
@@ -32,31 +33,31 @@ def get_password(username):
         return 'python'
     return None
 
-    
+
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error':'Not found'}), 404)
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
-    
+
 @auth.error_handler
 def unauthorized():
     # instead of 401, return 403 error code to prevent login dialog
     return make_response(jsonify({'error': 'Unauthorized access'}), 403)
 
 
-@app.route('/todo/api/v1.0/tasks',methods=['GET'])
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
 @auth.login_required
 def get_tasks():
     return jsonify({'tasks': [make_public_task(task) for task in tasks]})
-    
+
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
 def get_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
         abort(404)
-    return jsonify({'task':make_public_task(task[0])})
-        
+    return jsonify({'task': make_public_task(task[0])})
+
 
 @app.route('/todo/api/v1.0/tasks', methods=['POST'])
 def create_task():
@@ -65,12 +66,13 @@ def create_task():
     task = {
         'id': tasks[-1]['id'] + 1,
         'title': request.json['title'],
-        'description' : request.json.get('description',""),
-        'done': False    
+        'description': request.json.get('description', ""),
+        'done': False
     }
     tasks.append(task)
     return jsonify({'task': make_public_task(task)}), 201
-    
+
+
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
@@ -78,9 +80,9 @@ def update_task(task_id):
         abort(404)
     if not request.json:
         abort(400)
-    #if 'title' in request.json and type(request.json['title']) != unicode:
+    # if 'title' in request.json and type(request.json['title']) != unicode:
     #    abort(400)
-    #if 'description' in request.json and type(request.json['description']) != unicode:
+    # if 'description' in request.json and type(request.json['description']) != unicode:
     #    abort(400)
     if 'done' in request.json and type(request.json['done']) is not bool:
         abort(400)
@@ -88,7 +90,7 @@ def update_task(task_id):
     task[0]['description'] = request.json.get('description', task[0]['description'])
     task[0]['done'] = request.json.get('done', task[0]['done'])
     return jsonify({'task': make_public_task(task[0])})
-    
+
 
 @app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
 def delete_task(task_id):
@@ -97,12 +99,13 @@ def delete_task(task_id):
         abort(404)
     tasks.remove(task[0])
     return jsonify({'result': True})
-    
+
+
 def make_public_task(task):
     new_task = {}
     for field in task:
         if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'],_external=True)
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
         else:
             new_task[field] = task[field]
     return new_task
--- gforce.py
+++ gforce.py
@@ -7,25 +7,28 @@ Created on Wed Jul  6 01:23:10 2016
 
 import matplotlib.pyplot as plt
 
-def drawgraph(x,y):
-    plt.plot(x,y,marker='o')
+
+def drawgraph(x, y):
+    plt.plot(x, y, marker='o')
     plt.xlabel('Distance in meters')
     plt.ylabel('Gravitational force in newtons')
     plt.title('Gravitational force and distance')
     plt.show()
-    
+
+
 def generate_F_r():
-    r = range(100,1001,50)
+    r = range(100, 1001, 50)
     F = []
-    G = 6.574*(10**-11)
-    m1=0.5
-    m2=1.5
-    
+    G = 6.574 * (10 ** -11)
+    m1 = 0.5
+    m2 = 1.5
+
     for dist in r:
-        force = G*(m1*m2)/(dist**2)
+        force = G * (m1 * m2) / (dist ** 2)
         F.append(force)
-        
-    drawgraph(r,F)
-    
+
+    drawgraph(r, F)
+
+
 if __name__ == '__main__':
     generate_F_r()
